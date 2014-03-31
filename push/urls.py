a435e67537cbaa3607b6190b3be6d06ac7d6460c
Clark DuVall <clark.duvall@gmail.com>
2014-03-31 08:14:05 -0700

    Pusher

--- push/urls.py
+++ push/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from push import views
 
 urlpatterns = patterns('',
-    url(r'^auth', views.auth, name='auth'),
+    url(r'^auth$', views.auth, name='auth'),
+    url(r'^m/(?P<channel>[\w-]+)$', views.message, name='message'),
 )
--- push/views.py
+++ push/views.py
@@ -6,6 +6,9 @@ from django.views.decorators.http import require_POST
 import pusher
 
 
+p = pusher.pusher_from_url()
+
+
 def json_response(data):
     return HttpResponse(json.dumps(data), content_type='application/json')
 
@@ -15,7 +18,17 @@ def auth(request):
     channel = request.POST.get('channel_name')
     socket_id = request.POST.get('socket_id')
 
-    p = pusher.pusher_from_url()
-
     auth = p[channel].authenticate(socket_id)
     return json_response(auth)
+
+
+@require_POST
+def message(request, channel):
+    message = request.POST.get('message')
+
+    p['private-%s' % channel].trigger('message', {
+        'message': message,
+        'strength': 0.1
+    })
+
+    return json_response({'success': True})
